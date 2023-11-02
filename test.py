e87ce03a97428dba12098edbca524de9f7fe8793
Safecar28 <nealnpatelthegreat@gmail.com>
2023-11-02 11:49:39 -0700

    Changed GPIO pin from 37 to 35 for LED output

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 import time
 GPIO.cleanup()
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(37, GPIO.OUT)
-GPIO.output(37, GPIO.HIGH)
+GPIO.setup(35, GPIO.OUT)
+GPIO.output(35, GPIO.HIGH)
 time.sleep(10)
-GPIO.output(37, GPIO.LOW)
+GPIO.output(35, GPIO.LOW)
