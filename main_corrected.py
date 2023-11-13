a3a29be7c2f668a91dda7d1e7d420b995027cf54
Safecar28 <nealnpatelthegreat@gmail.com>
2023-11-13 11:59:54 -0700

    Update touch sensor variable name to button and print button state instead of touch state.

--- main_corrected.py
+++ main_corrected.py
@@ -13,13 +13,11 @@ ULTRASONIC_2_ECHO = 24
 RELAY_1 = 21 # Relay 1 is for ultrasonic 1
 RELAY_2 = 26 # Relay 2 is for RGB sensor
 BUZZER = 7 # Buzzer is for ultrasonic 2
-TOUCH = 16 # Touch sensor to toggle system on/off
+BUTTON = 16 # Touch sensor to toggle system on/off
 
-touch_state = 0
+button_state = 0
 
-a = 0
-
-print(touch_state)
+print(button_state)
 
 def setup():
     global sensor
@@ -39,20 +37,20 @@ def setup():
     GPIO.setup(RELAY_1, GPIO.OUT)
     GPIO.setup(RELAY_2, GPIO.OUT)
     GPIO.setup(BUZZER, GPIO.OUT)
-    GPIO.setup(TOUCH, GPIO.IN, pull_up_down=GPIO.PUD_UP) # pull up to high level
+    GPIO.setup(BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP) # pull up to high level
     # GPIO.output(RELAY_2, GPIO.HIGH)
 
     # PWM setup for buzzer
     Buzz = GPIO.PWM(BUZZER, 440)  # 440Hz frequency
 
 def detect(state):
-    global touch_state
-    if state != touch_state:
+    global button_state
+    if state != button_state:
         if state == 0:
             print("Touch switch is currently released.")
         if state == 1:
             print("Touch switch is currently pressed.")
-        touch_state = state
+        button_state = state
 
 def distance1(TRIG = ULTRASONIC_1_TRIG, ECHO = ULTRASONIC_1_ECHO):
     GPIO.output(TRIG, 0)
@@ -91,34 +89,35 @@ def distance2(TRIG = ULTRASONIC_2_TRIG, ECHO = ULTRASONIC_2_ECHO):
     return during * 340 / 2 * 100
 
 def loop():
-    global touch_state
-    global a
+    global button_state
     while True:
-        if touch_state == 0:
-            dis1 = distance1()
+        if button_state == 0:
             # dis1q = queue.Queue()
             # dis1th = threading.Thread(target=distance, args=(ULTRASONIC_1_TRIG, ULTRASONIC_1_ECHO, dis1q))
             # dis1th.start()
             # dis1th.join()
             # dis1 = dis1q.get() 
 
-            dis2 = distance2()
             # dis2q = queue.Queue()
             # dis2th = threading.Thread(target=distance, args=(ULTRASONIC_2_TRIG, ULTRASONIC_2_ECHO, dis2q))
             # dis2th.start()
             # dis2th.join()
             # dis2 = dis2q.get()
 
+            dis1 = distance1()
             print(f"Distance 1: {dis1} cm")
-            print(f"Distance 2: {dis2} cm")
-
-            if dis1 < 50:
+            
+            if dis1 < 50.0:
                 GPIO.output(RELAY_1, GPIO.HIGH)
                 print("Relay 1 on")
             else:
                 GPIO.output(RELAY_1, GPIO.LOW)
                 print("Relay 1 off")
-            if dis2 < 400:
+
+            dis2 = distance2()
+            print(f"Distance 2: {dis2} cm")
+
+            if dis2 < 400.0:
                 Buzz.start(50)
             else:
                 Buzz.stop()
@@ -135,18 +134,14 @@ def loop():
             print("Program will now wait for a second.")
             time.sleep(2)
             print("Checking...")
-            detect(GPIO.input(TOUCH))
-
-            a = 0
+            detect(GPIO.input(BUTTON))
         else:
-            if a == 0:
-                GPIO.output(RELAY_1, GPIO.LOW)
-                GPIO.output(RELAY_2, GPIO.LOW)
-                Buzz.stop()
-                a = 1
+            GPIO.output(RELAY_1, GPIO.LOW)
+            GPIO.output(RELAY_2, GPIO.LOW)
+            Buzz.stop()
             print("System turned off.")
             print("Checking...")
-            detect(GPIO.input(TOUCH))
+            detect(GPIO.input(BUTTON))
             time.sleep(0.1)
 
 def destroy():
