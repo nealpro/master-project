d46952790070018e6af0868e2f29405ea639710d
Safecar28 <nealnpatelthegreat@gmail.com>
2023-11-22 12:12:37 -0700

    Add test_webcolors.py with color conversion functions

---
+++ test_webcolors.py
@@ -0,0 +1,21 @@
+import webcolors
+
+def closest_color(requested_color):
+    min_colors = {}
+    for key, name in webcolors.CSS3_HEX_TO_NAMES.items():
+        r_c, g_c, b_c = webcolors.hex_to_rgb(key)
+        rd = (r_c - requested_color[0]) ** 2
+        gd = (g_c - requested_color[1]) ** 2
+        bd = (b_c - requested_color[2]) ** 2
+        min_colors[(rd + gd + bd)] = name
+    return min_colors[min(min_colors.keys())]
+
+def get_color_name(rgb_color):
+    try:
+        return webcolors.rgb_to_name(rgb_color)
+    except ValueError:
+        return closest_color(rgb_color)
+
+# Example
+color_name = get_color_name((130, 255, 150))
+print(color_name)
